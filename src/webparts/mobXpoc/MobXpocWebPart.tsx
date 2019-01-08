f66b1a83452e0dd8fbe93a671a251481187e1cbb
Dusan Zelenovic <djmrky@gmail.com>
2019-01-08 15:09:43 +0100

    initial commit

---
+++ src/webparts/mobXpoc/MobXpocWebPart.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
+import { BaseClientSideWebPart, IPropertyPaneConfiguration, PropertyPaneTextField, IWebPartContext } from "@microsoft/sp-webpart-base";
+import { Provider } from "mobx-react";
+
+import * as strings from "MobXpocWebPartStrings";
+import MobXpoc from "./containers/MobXpoc";
+import { IMobXpocProps } from "./containers/IMobXpocProps";
+import Store from "./store";
+
+export interface IMobXpocWebPartProps {
+  description: string;
+}
+
+export default class MobXpocWebPart extends BaseClientSideWebPart<IMobXpocWebPartProps> {
+  private store = new Store();
+
+  constructor(context: IWebPartContext) {
+    super(context);
+  }
+  public render(): void {
+    const element: React.ReactElement<IMobXpocProps> = React.createElement(MobXpoc, {
+      description: this.properties.description
+    });
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse("1.0");
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/mobXpoc/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMobXpocWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MobXpocWebPartStrings' {
+  const strings: IMobXpocWebPartStrings;
+  export = strings;
+}
